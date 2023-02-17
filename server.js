cc2dca509b3d418d0bc5632395683362facdeb3d
Rob Lehmann <lehmannrobb@gmail.com>
2023-02-17 16:40:00 +0300

    Set up cors origins

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+const corsOptions = {
+  origin: ["https://meal-planner-api.onrender.com"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
